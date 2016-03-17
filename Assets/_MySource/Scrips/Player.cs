d65f2a6d8fc2da6c30f313928a8fbd3200b58b9b
Alexander Eggers <alexander.eggers91@gmail.com>
2016-03-17 14:35:01 +0100

    Init project setup + added player + added input

---
+++ Assets/_MySource/Scrips/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Vector3 forceUp = new Vector3(0, 400, 0);
+	Vector3 defaultPos = new Vector3(0, 0, 0);
+	Rigidbody playerRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			playerRigidbody.velocity = defaultPos;
+			playerRigidbody.AddForce (forceUp);
+		}
+	}
+}
