cf6f97e0b67322204263ebc53ccac354c4b344fd
Alexander Eggers <alexander.eggers91@gmail.com>
2016-04-07 14:19:20 +0200

    Added bird animation

--- Assets/_MySource/Scripts/Player.cs
+++ Assets/_MySource/Scripts/Player.cs
@@ -6,15 +6,18 @@ public class Player : MonoBehaviour {
 	Vector3 forceUp = new Vector3(0, 300, 0);
 	Vector3 defaultPos = new Vector3(0, 0, 0);
 	Rigidbody playerRigidbody;
+    Animator animator;
 
 	// Use this for initialization
 	void Start () {
 		playerRigidbody = GetComponent<Rigidbody> ();
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space)) {
+            animator.SetBool("fly", true);
 			playerRigidbody.velocity = defaultPos;
 			playerRigidbody.AddForce (forceUp);
 		}
